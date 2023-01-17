5d4c88bc499703894818718c2cb1db47904b8ac3
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-01-17 19:17:48 +0100

    add orthographic camera instead of perspecitve camera

--- src/script.js
+++ src/script.js
@@ -25,7 +25,8 @@ scene.add(mesh)
 
 // Camera
 // Perpective camara field of view: between 45 and 75
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.OrthographicCamera(-1,1,1,-1,0.1,100)
 camera.position.x = 2
 camera.position.y = 2
 camera.position.z = 2
