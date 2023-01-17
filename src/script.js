be4493253c4a7c67652ac94fc078ff5c2655364e
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-01-17 20:45:13 +0100

    migrate lesson 4 to git repository

---
+++ src/script.js
@@ -0,0 +1,20 @@
+import './style.css'
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 0x008080})
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+const size = { with: 500, height: 500 }
+const camera = new THREE.PerspectiveCamera(75, size.with / size.height)
+camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+scene.add(camera)
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+renderer.setSize(size.with, size.height)
+renderer.render(scene, camera)
