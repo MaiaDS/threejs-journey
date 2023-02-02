a736174904185350dc687a90cc931aa219b86d69
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-02-02 19:34:53 +0100

    create many vertexes to have many triangles

--- src/script.js
+++ src/script.js
@@ -12,14 +12,24 @@ const scene = new THREE.Scene()
 
 // Object
 // const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const positionsArray = new Float32Array([
-    0,0,0,
-    0,1,0,
-    1,0,0
-])
+
+// const positionsArray = new Float32Array([
+//     0,0,0,
+//     0,1,0,
+//     1,0,0
+// ])
 // one vertex = 3 values
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+// const geometry = new THREE.BufferGeometry()
+// geometry.setAttribute('position', positionsAttribute)
 const geometry = new THREE.BufferGeometry()
+const count = 500
+const positionsArray = new Float32Array(count*3*3)
+// fill the array with random value
+for(let i = 0; i < count * 3 *3; i++) {
+    positionsArray[i] = (Math.random() - 0.5) *4
+}
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
 geometry.setAttribute('position', positionsAttribute)
 const material = new THREE.MeshBasicMaterial({ color: 0xe0e0e0, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
