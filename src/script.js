6bd982a9dd22b73c192a9d6d22672e999e1488f7
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-02-07 17:22:55 +0100

    style: change cube color

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: 0xe0e0e0 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
