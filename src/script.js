48f1f03420425bc7bfd2142094984ad46222640a
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-02-07 17:45:56 +0100

    use texture instead of color in material

--- src/script.js
+++ src/script.js
@@ -22,7 +22,7 @@ const scene = new THREE.Scene()
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xe0e0e0 })
+const material = new THREE.MeshBasicMaterial({ map: texture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
