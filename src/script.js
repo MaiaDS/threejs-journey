7146b337b88b41f50c7ab8070f89210c8be3d251
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-02-09 14:17:00 +0100

    create 3 object, position them and change color

--- src/script.js
+++ src/script.js
@@ -1,18 +1,23 @@
 import * as THREE from 'three'
+import { PlaneGeometry, SphereGeometry } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-/**
- * Base
- */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Sizes
- */
+// Objects
+const material = new THREE.MeshBasicMaterial({color: 0xe0e0e0})
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5,16,16), material)
+sphere.position.x = -1.5
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1,1), material)
+const torus = new THREE.Mesh(new THREE.TorusGeometry(0.5,0.2,16,32), material)
+torus.position.x = 1.5
+scene.add(sphere, plane, torus)
+
+// Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -33,10 +38,7 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
@@ -47,18 +49,14 @@ scene.add(camera)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+// Animate
 const clock = new THREE.Clock()
 
 const tick = () =>
