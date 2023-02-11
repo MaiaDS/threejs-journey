d8050dc332f171966feb87734dff353d32b98199
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-02-11 14:53:26 +0100

    center text

--- src/script.js
+++ src/script.js
@@ -35,6 +35,11 @@ fontLoader.load('/fonts/OverpassBlack.json', (font) => {
         bevelSegments: 4
     })
     textGeometry.computeBoundingBox()
+    textGeometry.translate(
+        -textGeometry.boundingBox.max.x * 0.5, 
+        -textGeometry.boundingBox.max.y * 0.5, 
+        -textGeometry.boundingBox.max.z * 0.5
+    )
     const textMaterial = new THREE.MeshBasicMaterial({wireframe:true})
     const text = new THREE.Mesh(textGeometry, textMaterial)
     scene.add(text)
