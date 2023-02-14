38c485988537a0a895a16989143a4e833acb4035
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-02-14 15:47:13 +0100

    orientate spot light

--- src/script.js
+++ src/script.js
@@ -40,9 +40,10 @@ rectAreaLight.position.set(-1.5,0,1.5)
 rectAreaLight.lookAt(new THREE.Vector3())
 // scene.add(rectAreaLight)
 
-const spotLight = new THREE.SpotLight(0x78ff00, 0.5,10,Math.PI * 0.1,0.25,1)
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.2, 0.25, 1) // color, intensity, distance, angle, penumbra, decay
 spotLight.position.set(0,2,3)
-scene.add(spotLight)
+spotLight.target.position.x = -0.75
+scene.add(spotLight,spotLight.target)
 
 /**
  * Objects
