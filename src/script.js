9141371c66a609d516df249901918731d73d1688
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-02-14 15:21:41 +0100

    add ambient light

--- src/script.js
+++ src/script.js
@@ -17,14 +17,9 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight(0xffffff,0.5) // parameters: colors, intensity
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.1)
 scene.add(ambientLight)
-
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
 
 /**
  * Objects
