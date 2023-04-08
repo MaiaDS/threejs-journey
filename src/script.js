e54b8260c02463fde36a63c7844e70005fe4b5e5
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-04-08 15:47:35 +0200

    create material and points

--- src/script.js
+++ src/script.js
@@ -16,7 +16,8 @@ const scene = new THREE.Scene()
 
 // Galaxy
 const parameters = {
-    count: 1000
+    count: 1000,
+    size: 0.02
 }
 
 const generateGalaxy = () => {
@@ -25,23 +26,26 @@ const generateGalaxy = () => {
     const positions = new Float32Array(parameters.count * 3)
     for(let i = 0; i < parameters.count; i++){
         const i3 = i * 3
-        positions[i3] = Math.random()
-        positions[i3 + 1] = Math.random()
-        positions[i3 + 2] = Math.random()
+        positions[i3] = (Math.random() - 0.5) * 3
+        positions[i3 + 1] =( Math.random() - 0.5) * 3
+        positions[i3 + 2] = (Math.random() - 0.5) * 3
     }
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    
+    // Material
+    const material = new THREE.PointsMaterial({
+        size:parameters.size, 
+        sizeAttenuation: true, 
+        depthWrite: false,
+        blending: THREE.AdditiveBlending
+    })
+
+    // Points
+    const points = new THREE.Points(geometry, material)
+    scene.add(points)
 }
 
 generateGalaxy()
-
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
 
 /**
  * Sizes
