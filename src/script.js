714f67f59e69c32059b1e248d65c927ed7b6fe5f
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-04-09 10:30:19 +0200

    create branches

--- src/script.js
+++ src/script.js
@@ -17,7 +17,9 @@ const scene = new THREE.Scene()
 // Galaxy
 const parameters = {
     count: 1000,
-    size: 0.02
+    size: 0.02,
+    radius: 5,
+    branches: 5
 }
 
 let geometry = null
@@ -31,15 +33,17 @@ const generateGalaxy = () => {
         material.dispose()
         scene.remove(points)
     }
-    
+
     // Geometry
     geometry = new THREE.BufferGeometry()
     const positions = new Float32Array(parameters.count * 3)
     for(let i = 0; i < parameters.count; i++){
         const i3 = i * 3
-        positions[i3] = (Math.random() - 0.5) * 3
-        positions[i3 + 1] =( Math.random() - 0.5) * 3
-        positions[i3 + 2] = (Math.random() - 0.5) * 3
+        const radius = Math.random() * parameters.radius
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+        positions[i3] = Math.cos(branchAngle) * radius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = Math.sin(branchAngle) * radius
     }
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
     
@@ -60,6 +64,8 @@ generateGalaxy()
 
 gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
 gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
 
 /**
  * Sizes
