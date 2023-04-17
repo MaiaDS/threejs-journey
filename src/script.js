558e57d36cfc0e3666ca0c6a599e38e2635158b8
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-04-17 15:49:47 +0200

    add triggered rotation

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import gsap from 'gsap'
 
 /**
  * Debug
@@ -135,8 +136,23 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // Scroll
 let scrollY = window.scrollY
+let currentSection = 0
+
 window.addEventListener('scroll', () => {
     scrollY = window.scrollY
+    const newSection = Math.round(scrollY / sizes.height)
+    if(currentSection != newSection){
+        currentSection = newSection
+        gsap.to(
+            sectionMeshes[currentSection].rotation,
+            {
+                duration: 1.5,
+                ease: 'power2.inOut',
+                x: '+=6',
+                y: '+=3' 
+            }
+        )
+    }
 })
 
 // Cursor 
@@ -163,8 +179,8 @@ const tick = () =>
 
     // Animate Meshes
     for(const mesh of sectionMeshes){
-        mesh.rotation.x = elapsedTime * 0.1
-        mesh.rotation.y = elapsedTime * 0.12
+        mesh.rotation.x += deltaTime * 0.1
+        mesh.rotation.y += deltaTime * 0.12
     }
 
     // Update the camera
