bfe50e90eff5efde2b2d6aadd0efe54e1af76251
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-04-17 14:35:01 +0200

    create material

--- src/script.js
+++ src/script.js
@@ -29,18 +29,11 @@ const scene = new THREE.Scene()
 //     new THREE.MeshBasicMaterial({ color: '#ff0000' })
 // )
 // scene.add(cube)
-const mesh1 = new THREE.Mesh(
-    new THREE.TorusGeometry(1,0.4,16,60),
-    new THREE.MeshBasicMaterial({color:'#ff0000'})
-)
-const mesh2 = new THREE.Mesh(
-    new THREE.ConeGeometry(1,2,32),
-    new THREE.MeshBasicMaterial({color:'#ff0000'})
-)
-const mesh3 = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.8,0.35,100,16),
-    new THREE.MeshBasicMaterial({color:'#ff0000'})
-)
+const material = new THREE.MeshToonMaterial({color: parameters.materialColor})
+
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1,0.4,16,60), material)
+const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1,2,32), material)
+const mesh3 = new THREE.Mesh(new THREE.TorusKnotGeometry(0.8,0.35,100,16), material)
 scene.add(mesh1,mesh2,mesh3)
 
 /**
