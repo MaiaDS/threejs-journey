2b0adb661ed06d805877be843046ff693cac1af6
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-04-17 15:37:28 +0200

    add particles

--- src/script.js
+++ src/script.js
@@ -59,6 +59,27 @@ scene.add(mesh1,mesh2,mesh3)
 
 const sectionMeshes = [mesh1,mesh2,mesh3]
 
+// Particles
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
+for (let i = 0; i < particlesCount; i++){
+    positions[i*3] = (Math.random() - 0.5) * 10
+    positions[i*3 + 1] =  objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
+    positions[i*3 + 2] = (Math.random() - 0.5) * 10
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+})
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
 // Light
 const directionalLight = new THREE.DirectionalLight(parameters.lightColor,1)
 directionalLight.position.set(1,1,0)
