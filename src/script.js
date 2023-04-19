c3e9955668f3215b251056d6db186fc9ea9927ed
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-04-19 11:07:41 +0200

    add reset function to gui panel

--- src/script.js
+++ src/script.js
@@ -33,6 +33,19 @@ debugObject.createBox = () =>
     )
 }
 gui.add(debugObject, 'createBox')
+
+// Reset
+debugObject.reset = () =>
+{
+    for(const object of objectsToUpdate) {
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+        scene.remove(object.mesh)
+    }
+    objectsToUpdate.splice(0, objectsToUpdate.length)
+}
+gui.add(debugObject, 'reset')
+
 /**
  * Base
  */
