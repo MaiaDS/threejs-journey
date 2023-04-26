d4b0cd96031e3026673fc75efea95d4299db1623
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-04-26 16:25:46 +0200

    cast a ray

--- src/script.js
+++ src/script.js
@@ -44,6 +44,11 @@ const rayOrigin = new THREE.Vector3(-3,0,0)
 const rayDirection = new THREE.Vector3(10,0,0).normalize()
 raycaster.set(rayOrigin, rayDirection)
 
+const intersect = raycaster.intersectObject(object2)
+console.log(intersect)
+const intersects = raycaster.intersectObjects([object1,object2,object3])
+console.log(intersects)
+
 /**
  * Sizes
  */
