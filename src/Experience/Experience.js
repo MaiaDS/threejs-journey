8db3460ea9ec287eb864e803f71127d91d0d4073
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-04-30 18:34:24 +0200

    Useful classes - Sizes

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,7 +1,10 @@
+import Sizes from './Utils/Sizes.js'
+
 export default class Experience {
     constructor(canvas){
         // console.log('Here starts a great experience')
         // window.experience = this
         this.canvas = canvas
+        this.sizes = new Sizes()
     }
 }
