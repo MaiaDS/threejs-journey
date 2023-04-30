de1c7e547a4b9853c5918395a13490a82930fcef
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-04-30 18:41:04 +0200

    Event Emitter

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,10 +1,15 @@
 import Sizes from './Utils/Sizes.js'
 
 export default class Experience {
-    constructor(canvas){
+    constructor(canvas) {
         // console.log('Here starts a great experience')
         // window.experience = this
         this.canvas = canvas
         this.sizes = new Sizes()
+        this.sizes.on('resize', () => {
+            this.resize()
+        })
     }
+
+    resize() {}
 }
