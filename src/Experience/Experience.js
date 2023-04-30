ccb81a91eb99f7854762f447a4925ad6378bb57c
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-04-30 18:31:25 +0200

    initiate experience class

---
+++ src/Experience/Experience.js
@@ -0,0 +1,7 @@
+export default class Experience {
+    constructor(canvas){
+        // console.log('Here starts a great experience')
+        // window.experience = this
+        this.canvas = canvas
+    }
+}
