3d67b45f2d3da19655ce19211c17cdf17d9373ac
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-02 19:09:23 +0200

    Pattern 41

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -80,6 +80,7 @@ void main() {
         vec2 wavedUv = vec2(vUv.x + sin(vUv.y * 100.0) * 0.1, vUv.y + sin(vUv.x * 100.0) * 0.1);
         float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25)); 
     */
-    float strength = atan(vUv.x, vUv.y);
+    // float strength = atan(vUv.x, vUv.y); // Pattern 40
+    float strength = atan(vUv.x - 0.5, vUv.y - 0.5); // Pattern 41
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
