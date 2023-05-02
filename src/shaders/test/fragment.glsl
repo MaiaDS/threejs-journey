06e1634e93b263cc4989ff5b226cc4e1af0459e2
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-02 18:44:52 +0200

    Pattern 24

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -35,6 +35,7 @@ void main() {
     // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5))) * (1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)))); // Pattern 20
     // float strength = floor(vUv.x * 10.0) / 10.0; // Pattern 21
     // float strength = floor(vUv.x * 10.0) / 10.0 * floor(vUv.y * 10.0) / 10.0; // Pattern 22
-    float strength = random(vUv); // Pattern 23
+    // float strength = random(vUv); // Pattern 23
+    float strength = random(vec2(floor(vUv.x * 10.0) / 10.0, floor(vUv.y * 10.0) / 10.0));
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
