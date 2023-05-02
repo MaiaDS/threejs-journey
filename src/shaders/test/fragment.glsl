f627b5826991f45b5bdabefdf8078d88493eb354
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-02 19:25:13 +0200

    Fix the strength

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -142,6 +142,6 @@ void main() {
 
     vec3 blackColor = vec3(0.0);
     vec3 uvColor = vec3(vUv, 1.0);
-    vec3 mixedColor = mix(blackColor, uvColor, strength);
+    vec3 mixedColor = mix(blackColor, uvColor, clamp(strength, 0.0, 1.0));
     gl_FragColor = vec4(mixedColor, 1.0);
 }
