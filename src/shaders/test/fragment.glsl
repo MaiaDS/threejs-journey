0e39c6f486570104dc6d65b4ed65bc472991cc38
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-02 18:20:26 +0200

    Pattern 8

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -7,6 +7,7 @@ void main() {
     // float strength = vUv.y; // Pattern 4
     // float strength = 1.0 - vUv.y; // Pattern 5
     // float strength = vUv.y * 10.0; // Pattern 6
-    float strength = mod(vUv.y * 10.0, 1.0);
+    // float strength = mod(vUv.y * 10.0, 1.0); // Pattern 7
+    float strength = step(0.5,(mod(vUv.y * 10.0, 1.0))); // Pattern 8
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
