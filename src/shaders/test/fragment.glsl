9f8084edd85fd5162a1586bbd3108685bc0a3bbe
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-02 18:15:26 +0200

    Pattern 4

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,8 +1,9 @@
 varying vec2 vUv;
 
 void main() {
-    // gl_FragColor = vec4(vUv, 1.0, 1.0);
-    // gl_FragColor = vec4(vUv, 0.0, 1.0);
-    float strength = vUv.x;
+    // gl_FragColor = vec4(vUv, 1.0, 1.0); // Pattern 1
+    // gl_FragColor = vec4(vUv, 0.0, 1.0); // Pattern 2
+    // float strength = vUv.x; // Pattern 3
+    float strength = vUv.y;
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
