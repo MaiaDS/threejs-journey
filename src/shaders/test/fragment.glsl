4d76cde00c399571591684e125d764c687bd7da2
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-02 18:32:13 +0200

    Pattern 15

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -15,8 +15,12 @@ void main() {
     // float strength = step(0.8,(mod(vUv.x * 10.0, 1.0))) * step(0.8, mod(vUv.y * 10.0, 1.0)); // Pattern 12
     // float strength = step(0.4,(mod(vUv.x * 10.0, 1.0))) * step(0.8, mod(vUv.y * 10.0, 1.0)); // Pattern 13
     // Pattern 14
-    float X = step(0.4,(mod(vUv.x * 10.0, 1.0))) * step(0.8, mod(vUv.y * 10.0, 1.0)); 
-    float Y = step(0.8,(mod(vUv.x * 10.0, 1.0))) * step(0.4, mod(vUv.y * 10.0, 1.0)); 
+    // float X = step(0.4,(mod(vUv.x * 10.0, 1.0))) * step(0.8, mod(vUv.y * 10.0, 1.0)); 
+    // float Y = step(0.8,(mod(vUv.x * 10.0, 1.0))) * step(0.4, mod(vUv.y * 10.0, 1.0)); 
+    // float strength = X + Y; 
+    // Pattern 15
+    float X = step(0.4,(mod(vUv.x * 10.0 - 0.2, 1.0))) * step(0.8, mod(vUv.y * 10.0, 1.0)); 
+    float Y = step(0.8,(mod(vUv.x * 10.0, 1.0))) * step(0.4, mod(vUv.y * 10.0 - 0.2, 1.0)); 
     float strength = X + Y; 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
