11c55df3e207baf748b0e5cc0aa5e5e7bba47a5e
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-03 10:02:00 +0200

    Big waves - elevation

--- src/script.js
+++ src/script.js
@@ -25,8 +25,13 @@ const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
     vertexShader: waterVertexShader,
-    fragmentShader: waterFragmentShader
+    fragmentShader: waterFragmentShader,
+    uniforms: {
+        uBigWavesElevation: { value: 0.2 }
+    }
 })
+
+gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name('uBigWavesElevation')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
