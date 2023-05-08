c4582d6b2a51036bf8aa469572f2e4802379ce6e
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-08 17:52:15 +0200

    Drawing our particle pattern - Disc pattern

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,8 @@
 void main() {
-    gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
+    // gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
+
+    // Disc
+    float strength = 1.0 - step(0.5, distance(gl_PointCoord, vec2(0.5)));
+    gl_FragColor = vec4(vec3(strength), 1.0);
+
 }
