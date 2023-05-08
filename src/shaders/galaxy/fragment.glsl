36cd33105d5e0474bb32906a0f9e9f28bcf6df24
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-08 17:54:53 +0200

    Drawing our particle pattern - Diffuse point pattern

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,8 +1,9 @@
 void main() {
     // gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
+    
+    // float strength = 1.0 - step(0.5, distance(gl_PointCoord, vec2(0.5))); // Disc
+    float strength = 1.0 - (2.0 * distance(gl_PointCoord, vec2(0.5))); // Diffuse point
 
-    // Disc
-    float strength = 1.0 - step(0.5, distance(gl_PointCoord, vec2(0.5)));
     gl_FragColor = vec4(vec3(strength), 1.0);
 
 }
