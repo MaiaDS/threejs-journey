dafc67002f5e488a4d01a633e71b5b6244d0dbf8
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-08 18:00:21 +0200

    Handling the color

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,4 @@
+varying vec3 vColor;
 void main() {
     // gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
     
@@ -5,6 +6,8 @@ void main() {
     // float strength = 1.0 - (2.0 * distance(gl_PointCoord, vec2(0.5))); // Diffuse point
     float strength = pow((1.0 - distance(gl_PointCoord, vec2(0.5))), 10.0); // Light point
 
-    gl_FragColor = vec4(vec3(strength), 1.0);
+    // Final color
+    vec3 color = mix(vec3(0.0), vColor, strength);
+    gl_FragColor = vec4(color, 1.0);
 
 }
