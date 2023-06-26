1f30b33754ff063beadd5630acd36e3eda2c72fc
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-06-26 15:54:00 +0200

    background blurriness and intensity

--- src/script.js
+++ src/script.js
@@ -65,6 +65,10 @@ const environmentMap = cubeTextureLoader.load([
     '/environmentMaps/0/nz.png'
 ])
 
+scene.backgroundBlurriness = 0
+scene.backgroundIntensity = 1
+gui.add(scene, 'backgroundBlurriness').min(0).max(1).step(0.001)
+gui.add(scene, 'backgroundIntensity').min(0).max(10).step(0.001)
 scene.environment = environmentMap
 scene.background = environmentMap
 
